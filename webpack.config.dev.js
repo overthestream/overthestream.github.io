76ad3c2ea8a30f76204742b474c7f58c0720ee2f
overthestream <njhnjh02@naver.com>
2021-02-14 17:01:38 +0900

    fix: use single quote

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,33 +1,33 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
-  context: path.resolve(__dirname, "."),
+  context: path.resolve(__dirname, '.'),
 
   entry: {
-    main: "./src/index.tsx",
+    main: './src/index.tsx',
   },
 
   output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "[name].[contenthash].bundle.js",
-    chunkFilename: "[id].chunk.js",
-    publicPath: "/",
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].[contenthash].bundle.js',
+    chunkFilename: '[id].chunk.js',
+    publicPath: '/',
   },
 
   devServer: {
-    contentBase: path.resolve(__dirname, "build"),
-    index: "index.html",
+    contentBase: path.resolve(__dirname, 'build'),
+    index: 'index.html',
     port: 8080,
     historyApiFallback: true,
   },
 
   resolve: {
     modules: [
-      path.resolve(__dirname, "src"),
-      path.resolve(__dirname, "node_modules"),
+      path.resolve(__dirname, 'src'),
+      path.resolve(__dirname, 'node_modules'),
     ],
-    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
   },
 
   module: {
@@ -49,15 +49,15 @@ const config = {
       },
       {
         test: /\.(ts|js)x?$/,
-        include: path.resolve(__dirname, "src"),
+        include: path.resolve(__dirname, 'src'),
         exclude: /node_modules/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: "html-loader",
+            loader: 'html-loader',
             options: {
               minimize: false,
             },
@@ -85,9 +85,9 @@ const config = {
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
-      filename: "index.html",
-      inject: "body",
+      template: './public/index.html',
+      filename: 'index.html',
+      inject: 'body',
     }),
   ],
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,27 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 const config = {
-  context: path.resolve(__dirname, "."),
+  context: path.resolve(__dirname, '.'),
 
   entry: {
-    main: "./src/index.tsx",
+    main: './src/index.tsx',
   },
 
   output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "[name].[contenthash].bundle.js",
-    chunkFilename: "[id].chunk.js",
-    publicPath: "/",
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].[contenthash].bundle.js',
+    chunkFilename: '[id].chunk.js',
+    publicPath: '/',
   },
 
   resolve: {
     modules: [
-      path.resolve(__dirname, "src"),
-      path.resolve(__dirname, "node_modules"),
+      path.resolve(__dirname, 'src'),
+      path.resolve(__dirname, 'node_modules'),
     ],
-    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
   },
 
   module: {
@@ -51,7 +51,7 @@ const config = {
         test: /\.html$/,
         use: [
           {
-            loader: "html-loader",
+            loader: 'html-loader',
             options: {
               minimize: false,
             },
@@ -79,9 +79,9 @@ const config = {
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
-      filename: "index.html",
-      inject: "body",
+      template: './public/index.html',
+      filename: 'index.html',
+      inject: 'body',
     }),
   ],
 
